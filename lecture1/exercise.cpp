84743fc23ed58bcfd55257b9384b04b4702dadb1
chat <1010922800@qq.com>
2024-12-19 22:00:09 +0800

    'new'

---
+++ lecture1/exercise.cpp
@@ -0,0 +1,35 @@
+/*手速练习
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "hello world" << endl;
+    return 0;
+}
+*/
+
+/*变量定义
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    int b, c = a, d = 10 / 2;
+    return 0;
+}
+*/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
+
+
+
+
+
+
+
